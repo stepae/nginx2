e8548a50f4a99a3b4ed08e60e08dde045ca79175
stepae <stepae@list.ru>
2020-06-15 16:32:14 +0300

    step2

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
+
+sudo rm /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo gunicorn hello:application -c etc/hello.py
+sudo gunicorn hello:application -c etc/hello.py &
+cd ask
+sudo gunicorn ask.wsgi:application -c ../etc/ask.py &

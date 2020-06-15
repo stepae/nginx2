ca2851ce32ec58b466c0abe8bb0b8ae3e836b632
stepae <stepae@list.ru>
2020-06-15 17:16:36 +0300

    change routing

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,13 +18,12 @@ from django.urls import path
 from qa.views import test
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', test),
-    path('login/', test),
-    path('signup/', test),
-    path('question/<int:id>/', test),
-    path('ask/', test),
-    path('popular/', test),
-    path('new/', test)
+ url(r'^$', 'test'), 
+ url(r'^login/.*$', 'test', name='login'), 
+ url(r'^signup/.*', 'test', name='signup'), 
+ url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'), 
+ url(r'^ask/.*', 'test', name='ask'), 
+ url(r'^popular/.*', 'test', name='popular'), 
+ url(r'^new/.*', 'test', name='new'),
     	
 ]
